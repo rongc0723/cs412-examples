e6f669f43c7c308fc87fcdd4c21348c4152001ef
Rong Chen <rongc0723@gmail.com>
2024-10-13 15:53:24 -0400

    task 2

--- mini_fb/models.py
+++ mini_fb/models.py
@@ -12,3 +12,17 @@ class Profile(models.Model):
 
     def __str__(self):
         return self.name
+
+    def get_status_messages(self):
+        '''Return all status messages for this profile and order by timestamp'''
+        status_messages = StatusMessage.objects.filter(profile=self).order_by('-timestamp')
+        return status_messages
+    
+
+class StatusMessage(models.Model):
+    timestamp = models.DateTimeField(auto_now_add=True)
+    message = models.TextField(blank=False)
+    profile = models.ForeignKey(Profile, on_delete=models.CASCADE)
+    
+    def __str__(self):
+        return f'{self.message} by {self.profile.name} at {self.timestamp}'
